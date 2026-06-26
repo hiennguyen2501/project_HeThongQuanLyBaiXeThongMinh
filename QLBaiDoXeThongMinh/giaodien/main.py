ef4d40dabe364fe92b07b6c4a5bb0de842493f78
hiennguyen2501 <hienng250104@gmail.com>
2026-06-26 10:03:58 +0700

    chỉnh final

--- QLBaiDoXeThongMinh/giaodien/main.py
+++ QLBaiDoXeThongMinh/giaodien/main.py
@@ -1,9 +1,18 @@
 from PyQt5.QtWidgets import QApplication
+from datetime import datetime, timedelta
+import os
 import sys
 
-from du_lieu_mau import tao_parking_service_mau
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if PROJECT_DIR not in sys.path:
+    sys.path.append(PROJECT_DIR)
+
+from doituong.loai_xe import OTo, XeMay
+from dulieu.du_lieu_mau import DANH_SACH_XE_MAU
 from presenters.trang_chu_presenter import TrangChuPresenter
+from services.parking_service import ParkingService
 from views.trang_chu import TrangChu
+from xulyhethong.quan_ly_bai_xe import QuanLyBaiXe
 
 
 APP_STYLE = """
@@ -27,6 +36,18 @@ QMessageBox QPushButton:hover {
     background-color: #E6EDF6;
 }
 """
+
+
+def tao_parking_service_mau():
+    bai_xe = QuanLyBaiXe()
+    thoi_gian_goc = datetime.now().replace(second=0, microsecond=0)
+
+    for index, (bien_so, loai_xe) in enumerate(DANH_SACH_XE_MAU):
+        thoi_gian_vao = thoi_gian_goc - timedelta(minutes=10 * (index + 1))
+        xe = XeMay(bien_so, thoi_gian_vao) if loai_xe == "XeMay" else OTo(bien_so, thoi_gian_vao)
+        bai_xe.check_in_xe(xe, dua_vao_hang_doi=False)
+
+    return ParkingService(bai_xe)
 
 
 def main():
